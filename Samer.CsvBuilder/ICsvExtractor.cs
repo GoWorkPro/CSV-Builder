85344af8c102579c00025ac4ac072721bb0380de
Samer Shahbaz <samer.shahbaz@softsteer.com>
2023-12-12 13:01:42 +0500

    //

--- Samer.CsvBuilder/ICsvExtractor.cs
+++ Samer.CsvBuilder/ICsvExtractor.cs
@@ -12,22 +12,5 @@ namespace GoWorkPro.CsvBuilder
     {
         MemoryStream GetStream();
         void SaveAsFile(string filePath);
-
-        DataTable[] ToDataTables(KeyValuePair<Func<ReadCriteria, bool>, Func<ReadCriteria, bool>>[] startAndEndCriterias);
-
-        /// <summary>
-        /// It expects the column number and row number to modify value at specific point, column number and row number start At 1
-        /// </summary>
-        /// <param name="columnNumber"></param>
-        /// <param name="rowNumber"></param>
-        /// <param name="value"></param>
-        void SetValue<T>(int columnNumber, int rowNumber, T value);
-        T GetValue<T>(int columnNumber, int rowNumber);
-        object[] GetRowValues(int rowNumber);
-        /// <summary>
-        /// Update row at specific row number, row number start At 1
-        /// </summary>
-        /// <param name="rowNumber"></param>
-        void SetRow(int rowNumber, object[] values);
     }
 }
