05085e944e596bbdf91f6dfc5d008800caf93efa
Samer Shahbaz <samer.shahbaz@softsteer.com>
2023-12-21 11:31:50 +0500

    //

--- Samer.CsvBuilder/CsvBuilder.cs
+++ Samer.CsvBuilder/CsvBuilder.cs
@@ -121,7 +121,7 @@ namespace GoWorkPro.CsvBuilder
         {
             var rowsAfterSperators = rows.Select(x => string.Join(options.Separator, x)).Where(x => options.RemoveEmptyRows && !string.IsNullOrWhiteSpace(x) || !options.RemoveEmptyRows).Skip(options.SkipInitialNumberOfRows).ToArray();
             return new CsvBuilder(options, parseIntoTable(options, rowsAfterSperators));
-        }
+        } 
 
         public ICsvExtractor Build(params int[] columnsTobePresentedForTableIndex)
         {
